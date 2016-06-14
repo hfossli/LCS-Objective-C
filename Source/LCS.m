7bb16d3cb21c7a1bcbc713ed007b4d42e288d155
Håvard Fossli <hfossli@gmail.com>
2016-06-14 11:51:25 +0200

    Implementing cache for equality

--- Source/LCS.m
+++ Source/LCS.m
@@ -8,6 +8,13 @@
 
 #import "LCS.h"
 
+typedef NS_ENUM(NSUInteger, _LCSObjectEquallity)
+{
+    _LCSObjectEquallityUnknown,
+    _LCSObjectEquallityEqual,
+    _LCSObjectEquallityUnequal,
+};
+
 @implementation LCS
 
 + (NSIndexSet *)compareArray:(NSArray *)a
@@ -20,6 +27,7 @@
     NSUInteger aCount = a.count;
     NSUInteger bCount = b.count;
     NSInteger lengths[aCount+1][bCount+1];
+    _LCSObjectEquallity cache[aCount+1][bCount+1];
     
     for (NSInteger i = aCount; i >= 0; i--) {
         for (NSInteger j = bCount; j >= 0; j--) {
@@ -27,8 +35,10 @@
             if (i == aCount || j == bCount) {
                 lengths[i][j] = 0;
             } else if (objectComparison(a[i], b[j])) {
+                cache[i][j] = _LCSObjectEquallityEqual;
                 lengths[i][j] = 1 + lengths[i+1][j+1];
             } else {
+                cache[i][j] = _LCSObjectEquallityUnequal;
                 lengths[i][j] = MAX(lengths[i+1][j], lengths[i][j+1]);
             }
         }
@@ -36,8 +46,10 @@
     
     NSMutableIndexSet *commonIndexes = [NSMutableIndexSet indexSet];
     
-    for (NSInteger i = 0, j = 0 ; i < aCount && j < bCount; ) {
-        if (objectComparison(a[i], b[j])) {
+    for (NSInteger i = 0, j = 0 ; i < aCount && j < bCount;) {
+        _LCSObjectEquallity equality = cache[i][j];
+        NSAssert(equality == _LCSObjectEquallityUnequal || equality == _LCSObjectEquallityEqual, @"The cache should be filled up and these values should be the only valid values. Received %i", (int)equality);
+        if (equality == _LCSObjectEquallityEqual) {
             [commonIndexes addIndex:i];
             i++; j++;
         } else if (lengths[i+1][j] >= lengths[i][j+1]) {
@@ -62,15 +74,14 @@
         *removedIndexesPointer = removedIndexes;
     }
     
-    
     if (addedIndexesPointer) {
 
         NSUInteger indexes[commonIndexes.count];
         [commonIndexes getIndexes:indexes maxCount:commonIndexes.count inIndexRange:nil];
         
         NSMutableIndexSet *addedIndexes = [NSMutableIndexSet indexSet];
-        for (NSInteger i = 0, j = 0; i < commonIndexes.count || j < bCount; ) {
-            if (i < commonIndexes.count && j < bCount && objectComparison(a[indexes[i]], b[j])) {
+        for (NSInteger i = 0, j = 0; i < commonIndexes.count || j < bCount;) {
+            if (i < commonIndexes.count && j < bCount && cache[indexes[i]][j] == _LCSObjectEquallityEqual) {
                 i++;
                 j++;
             } else {
@@ -81,8 +92,6 @@
         
         *addedIndexesPointer = addedIndexes;
     }
-    
-    return commonIndexes;
 }
 
 @end
